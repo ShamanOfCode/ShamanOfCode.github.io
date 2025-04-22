295d3078ebe0e73b16e826213e4279e1e7f6adbd
ShamanOfCode <evgeniymarkov2015@gmail.com>
2025-04-22 21:08:23 +0200

    Started first lines

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <h1>Hello World!!</h1>
       </body>
     </html>
   );
